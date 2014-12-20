a4b2e0e0ec1c6a679308fde2e016750bb1e4b10c
Tully Foote <tfoote@osrfoundation.org>
2014-12-20 09:10:37 -0800

    use correct branch syntax

--- reconfigure.bash
+++ reconfigure.bash
@@ -4,7 +4,7 @@ set -o errexit
 
 BUILDFARM_DEPLOYMENT_PATH=/root/buildfarm_deployment
 BUILDFARM_DEPLOYMENT_URL=https://github.com/ros-infrastructure/buildfarm_deployment.git
-BUILDFARM_DEPLOYMENT_BRANCH=origin/master
+BUILDFARM_DEPLOYMENT_BRANCH=master
 
 if [ ! -d $1 ]; then
   echo "$1 is not a valid subdirectory"
@@ -27,6 +27,6 @@ cp $1/common.yaml /etc/puppet/hieradata
 
 
 echo "Asserting latest version of $BUILDFARM_DEPLOYMENT_URL as $BUILDFARM_DEPLOYMENT_BRANCH"
-cd $BUILDFARM_DEPLOYMENT_PATH && git fetch origin && git reset --hard $BUILDFARM_DEPLOYMENT_BRANCH
+cd $BUILDFARM_DEPLOYMENT_PATH && git fetch origin && git reset --hard origin/$BUILDFARM_DEPLOYMENT_BRANCH
 echo "Running puppet"
 puppet apply -v $BUILDFARM_DEPLOYMENT_PATH/$1/manifests/site.pp --modulepath=/etc/puppet/modules:/usr/share/puppet/modules:$BUILDFARM_DEPLOYMENT_PATH/$1 #-l /var/log/puppet.log
