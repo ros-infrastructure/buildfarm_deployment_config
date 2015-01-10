d5088a618b93a0225b0372b9a9607a311dd67411
Tully Foote <tfoote@osrfoundation.org>
2015-01-09 16:38:47 -0800

    make is needed

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -3,7 +3,7 @@
 set -o errexit
 
 apt-get update
-apt-get install -y ruby ruby1.9.1-dev
+apt-get install -y ruby ruby1.9.1-dev make
 gem install puppet librarian-puppet --no-rdoc --no-ri
 
 # config for librarian for more efficient syncronization
