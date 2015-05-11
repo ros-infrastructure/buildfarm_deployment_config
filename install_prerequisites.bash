13591ced8a55c3ca25a92a5cc84649dcb0bfd927
Tully Foote <tfoote@osrfoundation.org>
2015-05-11 16:32:34 -0700

    hold puppet version down to 3.7
    4.0 is now the default, but not everything is compatible

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -4,7 +4,9 @@ set -o errexit
 
 apt-get update
 apt-get install -y ruby ruby1.9.1-dev make
-gem install puppet librarian-puppet --no-rdoc --no-ri
+# hold puppet version down, not all modules work with 4.0
+gem install puppet -v 3.7 --no-rdoc --no-ri
+get installl librarian-puppet --no-rdoc --no-ri
 
 # config for librarian for more efficient syncronization
 librarian-puppet config rsync true --global
