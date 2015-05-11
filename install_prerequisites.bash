469acb6f558949ac0a7026a941737d9d6d43de80
Tully Foote <tfoote@osrfoundation.org>
2015-05-11 16:49:19 -0700

    fixing typo

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y ruby ruby1.9.1-dev make
 # hold puppet version down, not all modules work with 4.0
 gem install puppet -v 3.7 --no-rdoc --no-ri
-get installl librarian-puppet --no-rdoc --no-ri
+gem install librarian-puppet --no-rdoc --no-ri
 
 # config for librarian for more efficient syncronization
 librarian-puppet config rsync true --global
