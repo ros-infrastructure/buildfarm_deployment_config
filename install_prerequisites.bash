db42efc2163bcc8f5dbf2558eee8b960fe595d77
Tully Foote <tfoote@osrfoundation.org>
2015-05-11 17:54:24 -0700

    work around docker-py pip requests conflict in ros-infrastructure/buildfarm_deployment#64

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -8,5 +8,10 @@ apt-get install -y ruby ruby1.9.1-dev make
 gem install puppet -v 3.7 --no-rdoc --no-ri
 gem install librarian-puppet --no-rdoc --no-ri
 
+# install pip3 via pypi to avoid https://github.com/ros-infrastructure/buildfarm_deployment/issues/64
+
+apt-get install -y python3-pip
+pip3 install -U pip
+
 # config for librarian for more efficient syncronization
 librarian-puppet config rsync true --global
