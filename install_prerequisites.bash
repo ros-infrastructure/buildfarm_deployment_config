20ebd193cf1e25c78e3bcf63578d76f1d2ad0012
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-03 11:02:25 -0400

    Update master IP for testing xenial env.

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -5,7 +5,6 @@ set -o errexit
 apt-get install -y puppet librarian-puppet
 
 # install pip3 via pypi to avoid https://github.com/ros-infrastructure/buildfarm_deployment/issues/64
-
 apt-get install -y python3-pip
 pip3 install -U pip
 
