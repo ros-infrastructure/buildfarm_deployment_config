724a284aa7f9d5ffd046a562a2b5725fd758c9bd
Steven! Ragnarök <steven@nuclearsandwich.com>
2017-04-17 11:11:18 -0400

    Refactor hiera and role specification.
    With this change and the analogous one in the buildfarm_deployment
    repository the different high level roles are refactored from separate
    hiera configs and site manifests to a unified hiera hierarchy and role
    modules.
    
    The common.yaml hiera data is now actually common and at the base of the
    hierarchy. Now there is an extra level: buildfarm_role which is provided
    as a custom fact via the `FACTER_buildfarm_role` environment variable.
    This is one of agent, master, repo; analogous to the master, repo, slave
    directories containing hiera data and a site manifest.

--- reconfigure.bash
+++ reconfigure.bash
@@ -5,10 +5,12 @@ set -o errexit
 BUILDFARM_DEPLOYMENT_PATH=/root/buildfarm_deployment
 BUILDFARM_DEPLOYMENT_URL=https://github.com/nuclearsandwich/buildfarm_deployment.git
 BUILDFARM_DEPLOYMENT_BRANCH=xenialize
-if [ ! -d $1 ]; then
-  echo "$1 is not a valid subdirectory"
+if [ -z $1 ]; then
+  echo "No role specified."
   return 1
 fi
+
+buildfarm_role="$1"
 
 if [ ! -d /root/buildfarm_deployment ]; then
   echo "/root/buildfarm_deplyment did not exist, cloning."
@@ -17,20 +19,17 @@ fi
 
 echo "Copying in configuration"
 mkdir -p /etc/puppet/hieradata
-cp $1/hiera.yaml /etc/puppet
-cp $1/common.yaml /etc/puppet/hieradata
-
-
-
+cp hiera/hiera.yaml /etc/puppet/
+cp -r hiera/hieradata/* /etc/puppet/hieradata/
 
 echo "Asserting latest version of $BUILDFARM_DEPLOYMENT_URL as $BUILDFARM_DEPLOYMENT_BRANCH"
 cd $BUILDFARM_DEPLOYMENT_PATH && git fetch origin && git reset --hard origin/$BUILDFARM_DEPLOYMENT_BRANCH
 echo "Running librarian-puppet"
 (cd $BUILDFARM_DEPLOYMENT_PATH/ && librarian-puppet install --verbose)
 echo "Running puppet"
-puppet apply --verbose \
+env FACTER_buildfarm_role="$buildfarm_role" puppet apply --verbose \
   --parser future \
-  --modulepath=$BUILDFARM_DEPLOYMENT_PATH/$1:$BUILDFARM_DEPLOYMENT_PATH/modules \
+  --modulepath=$BUILDFARM_DEPLOYMENT_PATH \
   --logdest /var/log/puppet.log \
-  $BUILDFARM_DEPLOYMENT_PATH/$1/manifests/site.pp \
+  -e "include role::buildfarm::${buildfarm_role}" \
   || { r=$?; echo "puppet failed, please check /var/log/puppet.log, the last 10 lines are:"; tail -n 10 /var/log/puppet.log; exit $r; }
