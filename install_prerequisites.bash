eb99daf8a8efe2a6ebb7083469af0aabb05605d8
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2019-03-22 19:11:36 +0100

    Check for presence of cron deny/allow.
    And warn user if either of those is found. Cron setup for jenkins agents fails if these files are present.

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -20,3 +20,15 @@ pip3 install -U pip
 
 # config for librarian for more efficient syncronization
 librarian-puppet config rsync true --global
+
+
+# check cron setup (for https://github.com/ros-infrastructure/buildfarm_deployment/issues/187)
+echo -n "Checking cron setup .. "
+if [ -e /etc/cron.allow ] || [ -e /etc/cron.deny ];
+then
+  echo -e "found 'cron.allow' or 'cron.deny'.\n"
+  echo -e "Warning: jenkins agent crontab configuration may fail.\n"
+  echo -e "This may lead to problems with the jenkins agents.\n"
+else
+  echo "OK"
+fi
